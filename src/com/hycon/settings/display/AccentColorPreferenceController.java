i-o-n/packages_apps_ionizer

AccentColorPicker: set color preview from resources if default RGB value is set
Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

src/com/ion/ionizer/display/AccentColorPreferenceController.java
@@ -61,6 +61,9 @@ public class AccentColorPreferenceController extends AbstractPreferenceControlle
             mAccentColor.setSummary(hexColor);
         }
         mAccentColor.setNewPreviewColor(intColor);
+        if (hexColor.equals("#ff1a73e8"))
+            mAccentColor.setCustomColorPreview(mContext.getResources()
+                    .getColor(R.color.ion_icon_tint_color));
     }
 
     @Override

HyconOS/android_packages_apps_HyconHub

Signed-off-by: Joey Huab <joey@evolution-x.org>

src/com/hycon/settings/display/AccentColorPreferenceController.java
@@ -65,6 +65,9 @@ public class AccentColorPreferenceController extends AbstractPreferenceControlle
             mAccentColor.setSummary(hexColor);
         }
         mAccentColor.setNewPreviewColor(intColor);
+        if (hexColor.equals("#ff1a73e8"))
+            mAccentColor.setCustomColorPreview(mContext.getResources()
+                    .getColor(R.color.hycon_icon_tint_color));
     }
 
     @Override