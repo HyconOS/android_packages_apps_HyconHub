2c76e9bfb7524acea2c7be76fdc578a6103ab92f
Ido Ben-Hur <idoybh2@gmail.com>
2021-03-16 13:54:48 +0200

    AODSchedule: Add support for mixed time & sun modes [2/2]
    Also make the code a bit more readable
    
    Change-Id: Ibf2c4671693e745a2dfeaa07dbc6aeed1a6fe0e3

--- src/com/hycon/settings/Lockscreen.java
+++ src/com/hycon/settings/Lockscreen.java
@@ -45,6 +45,16 @@ import java.util.List;
 @SearchIndexable
 public class Lockscreen extends SettingsPreferenceFragment {
 
+    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
+
+    static final int MODE_DISABLED = 0;
+    static final int MODE_NIGHT = 1;
+    static final int MODE_TIME = 2;
+    static final int MODE_MIXED_SUNSET = 3;
+    static final int MODE_MIXED_SUNRISE = 4;
+
+    Preference mAODPref;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -67,15 +77,22 @@ public class Lockscreen extends SettingsPreferenceFragment {
         int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
                 Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
         switch (mode) {
-            case 0:
-                mAODPref.setSummary(R.string.disabled);
+            default:
+            case MODE_DISABLED:
                 break;
-            case 1:
+            case MODE_NIGHT:
                 mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
                 break;
-            case 2:
+            case MODE_TIME:
                 mAODPref.setSummary(R.string.night_display_auto_mode_custom);
                 break;
+            case MODE_MIXED_SUNSET:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunset);
+                break;
+            case MODE_MIXED_SUNRISE:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunrise);
+                break;
+        }
     }
 
     @Override
