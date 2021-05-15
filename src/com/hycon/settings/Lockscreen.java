3351d31d50fe68e5296eb386c9ecc93b167ff32d
idoybh <idoybh2@gmail.com>
2021-05-15 08:18:36 +0000

    Only allow edge lights on AOD when AOD is enabled [2/2]

--- src/com/hycon/settings/Lockscreen.java
+++ src/com/hycon/settings/Lockscreen.java
@@ -61,6 +61,7 @@ public class Lockscreen extends SettingsPreferenceFragment {
     private static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
     private static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
     private static final String PULSE_COLOR_MODE_PREF = "ambient_notification_light_color_mode";
+    private static final String KEY_AMBIENT = "ambient_notification_light_enabled";
 
     static final int MODE_DISABLED = 0;
     static final int MODE_NIGHT = 1;
@@ -74,6 +75,7 @@ public class Lockscreen extends SettingsPreferenceFragment {
     private SystemSettingSeekBarPreference mEdgeLightDurationPreference;
     private SystemSettingSeekBarPreference mEdgeLightRepeatCountPreference;
     private ListPreference mColorMode;
+    private SystemSettingSwitchPreference mAmbientPref;
 
     Preference mAODPref;
 
@@ -99,6 +101,15 @@ public class Lockscreen extends SettingsPreferenceFragment {
         mAmbientIconsColor.setNewPreviewColor(intColor);
         mAmbientIconsColor.setSummary(hexColor);
         mAmbientIconsColor.setOnPreferenceChangeListener(this);
+
+        mAmbientPref = (SystemSettingSwitchPreference) findPreference(KEY_AMBIENT);
+        boolean aodEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.DOZE_ALWAYS_ON, 0, UserHandle.USER_CURRENT) == 1;
+        if (!aodEnabled) {
+            mAmbientPref.setChecked(false);
+            mAmbientPref.setEnabled(false);
+            mAmbientPref.setSummary(R.string.aod_disabled);
+        }
 
         mEdgeLightRepeatCountPreference = (SystemSettingSeekBarPreference) findPreference(AMBIENT_LIGHT_REPEAT_COUNT);
         mEdgeLightRepeatCountPreference.setOnPreferenceChangeListener(this);
