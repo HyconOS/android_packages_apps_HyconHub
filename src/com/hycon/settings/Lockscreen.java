d2a39bf9d40c19d9be52c4d6531899eca12b2779
Ido Ben-Hur <idoybh2@gmail.com>
2021-05-15 06:48:37 +0000

    Allow scheduling always on display [2/2]

--- src/com/hycon/settings/Lockscreen.java
+++ src/com/hycon/settings/Lockscreen.java
@@ -52,6 +52,35 @@ public class Lockscreen extends SettingsPreferenceFragment {
         PreferenceScreen prefSet = getPreferenceScreen();
         final Resources res = getResources();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        mAODPref = findPreference(AOD_SCHEDULE_KEY);
+        updateAlwaysOnSummary();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateAlwaysOnSummary();
+    }
+
+    private void updateAlwaysOnSummary() {
+        if (mAODPref == null) return;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        switch (mode) {
+            case 0:
+                mAODPref.setSummary(R.string.disabled);
+                break;
+            case 1:
+                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
+                break;
+            case 2:
+                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
+                break;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 
     @Override
