1ab562bc3d42febe9418bfd64053da4b6daa8cac
spkal01 <kalligeross@gmail.com>
2021-07-20 13:10:07 +0530

    HyconHub: Introduce Brightness Slider Styles
    @Blaster4385: Adapt to HyconHub

--- src/com/hycon/settings/QuickSettings.java
+++ src/com/hycon/settings/QuickSettings.java
@@ -15,10 +15,29 @@
  */
 package com.hycon.settings;
 
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.AlertDialog;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.DialogInterface;
+import android.content.ServiceConnection;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.content.pm.PackageManager;
+import android.database.ContentObserver;
+import android.net.Uri;
+
+import android.os.SystemProperties;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -29,11 +48,16 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.*;
 
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.android.settings.custom.preference.SystemSettingListPreference;
+import com.android.settings.custom.preference.SystemSettingSwitchPreference;
+import com.android.internal.util.custom.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 
@@ -44,13 +68,130 @@ import java.util.List;
 @SearchIndexable
 public class QuickSettings extends SettingsPreferenceFragment {
 
+    private IOverlayManager mOverlayManager;
+    private PackageManager mPackageManager;    
+    private static final String SLIDER_STYLE  = "slider_style";
+
+    private SystemSettingListPreference mSlider;
+    private Handler mHandler;
+
     @Override
     public void onCreate(Bundle icicle) {
+        getActivity().getActionBar().hide();        
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.quick_settings);
         PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        Context mContext = getContext();
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mSlider = (SystemSettingListPreference) findPreference(SLIDER_STYLE);
+        mCustomSettingsObserver.observe();        
 
     }
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SLIDER_STYLE  ),
+                    false, this, UserHandle.USER_ALL);                  
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.SLIDER_STYLE  ))) {
+                updateSlider();
+            }
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSlider) {
+            mCustomSettingsObserver.observe();
+            return true;
+        }
+        return false;
+    }
+
+    private void updateSlider() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean sliderDefault = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 0;
+        boolean sliderOOS = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 1;
+        boolean sliderAosp = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 2;
+        boolean sliderRUI = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 3;
+        boolean sliderA12 = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 4;                
+
+        if (sliderDefault) {
+            setDefaultSlider(mOverlayManager);
+        } else if (sliderOOS) {
+            enableSlider(mOverlayManager, "com.android.theme.systemui_slider_oos");
+        } else if (sliderAosp) {
+            enableSlider(mOverlayManager, "com.android.theme.systemui_slider.aosp");
+        } else if (sliderRUI) {
+            enableSlider(mOverlayManager, "com.android.theme.systemui_slider.rui");
+        } else if (sliderA12) {
+            enableSlider(mOverlayManager, "com.android.theme.systemui_slider.a12");            
+            
+        }
+    }
+
+    public static void setDefaultSlider(IOverlayManager overlayManager) {
+        for (int i = 0; i < SLIDERS.length; i++) {
+            String sliders = SLIDERS[i];
+            try {
+                overlayManager.setEnabled(sliders, false, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void enableSlider(IOverlayManager overlayManager, String overlayName) {
+        try {
+            for (int i = 0; i < SLIDERS.length; i++) {
+                String sliders = SLIDERS[i];
+                try {
+                    overlayManager.setEnabled(sliders, false, USER_SYSTEM);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+            overlayManager.setEnabled(overlayName, true, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
+        try {
+            mOverlayManager.setEnabled(packagename,
+                    state, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String[] SLIDERS = {
+        "com.android.theme.systemui_slider_oos",
+        "com.android.theme.systemui_slider.aosp",
+        "com.android.theme.systemui_slider.rui",
+        "com.android.theme.systemui_slider.a12"        
+    };
 
     @Override
     public int getMetricsCategory() {
